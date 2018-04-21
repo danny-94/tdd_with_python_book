20230ec4eba227cf18a9810bd823553a5e9c68ee
dhanvanthri94 <dhanvanthri94@gmail.com>
2018-04-21 13:36:46 +0530

    First FT specced out in comments, and now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
-# browser = webdriver.Chrome(r'../chromedriver_linux64/chromedriver')
-browser = webdriver.Firefox()
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+import unittest
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        assert 'To-Do' in self.browser.title
+        self.fail('Finish the test!')
 # She is invited to enter a to-do item straight away
 # She types "Buy peacock feathers" into a text box (Edith's hobby
 # is tying fly-fishing lures)
@@ -19,4 +29,7 @@ assert 'To-Do' in browser.title
 # explanatory text to that effect.
 # She visits that URL - her to-do list is still there.
 # Satisfied, she goes back to sleep
-browser.quit()
+# browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
